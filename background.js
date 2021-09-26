568843580cccf75e538b6f9bb43e7bd20694479e
crabshank <44329154+crabshank@users.noreply.github.com>
2021-09-26 18:16:34 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -2,11 +2,19 @@ try {
 var addrs=[];
 
 function doneAddr(responseDetails) {
-	let filt=addrs.filter((adr)=>{return adr.url==responseDetails.url;});
+	let filt=addrs.filter((adr)=>{return (adr.url==responseDetails.url  && adr.tabId==responseDetails.tabId);});
 	if(filt.length==0){
 		addrs.push(responseDetails);
 	}
 }
+
+chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
+	for (let i = 0, len = addrs.length; i<len; i++){
+		if(addrs[i].tabId==removedTabId){
+			addrs[i].tabId=addedTabId;
+		}
+	}
+});
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 		if (!!changeInfo.url) {
@@ -16,7 +24,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 function sendImg(requestDetails) {
-		let filt=addrs.filter((adr)=>{return adr.url==requestDetails.url;});
+		let filt=addrs.filter((adr)=>{return (adr.url==requestDetails.url && adr.tabId==requestDetails.tabId);});
 	if(filt.length>0){
 		chrome.tabs.sendMessage(requestDetails.tabId, {imgSrc: requestDetails.url});
 	}
