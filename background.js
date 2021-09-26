ef61e818e5ae0ed75fa1c9ab8e9ff4899e118043
crabshank <44329154+crabshank@users.noreply.github.com>
2021-09-26 17:25:45 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,13 +1,37 @@
+var addrs=[];
+
 try {
-function sendImg(requestDetails) {
-chrome.tabs.sendMessage(requestDetails.tabId, {imgSrc: requestDetails.url});
+function doneAddr(responseDetails) {
+	let filt=addrs.filter((adr)=>{return adr.url==responseDetails.url;});
+	if(filt.length==0){
+		addrs.push(responseDetails);
+	}
 }
 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+		if (!!changeInfo.url) {
+			let filt=addrs.filter((adr)=>{return adr.tabId!=tabId;});
+			addrs=filt;
+		}
+});
+
+function sendImg(requestDetails) {
+		let filt=addrs.filter((adr)=>{return adr.url==requestDetails.url;});
+	if(filt.length>0){
+		chrome.tabs.sendMessage(requestDetails.tabId, {imgSrc: requestDetails.url});
+	}
+}
 
 chrome.webRequest.onBeforeRequest.addListener(
   sendImg,
   {urls:["<all_urls>"], types:["image"]},
   ["blocking","requestBody", "extraHeaders"]
+);
+
+chrome.webRequest.onCompleted.addListener(
+  doneAddr,
+  {urls:["<all_urls>"], types:["image"]},
+  ["responseHeaders"]
 );
 
 } catch (e) {
