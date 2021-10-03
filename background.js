d6e845cfb123a00bed2d2de704c62b2de1f1613c
crabshank <44329154+crabshank@users.noreply.github.com>
2021-10-03 17:16:00 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 try {
 var addrs=[];
 
-/*function doneAddr(responseDetails) {
-	let filt=addrs.filter((adr)=>{return (adr.url==responseDetails.url  && adr.tabId==responseDetails.tabId);});
+function sendImg(requestDetails) {
+	let filt=addrs.filter((adr)=>{return (adr.tabId==requestDetails.tabId && adr.url==requestDetails.url);});
 	if(filt.length==0){
-		addrs.push(responseDetails);
+		chrome.tabs.sendMessage(requestDetails.tabId, {imgSrc: requestDetails.url});
+		addrs.push(requestDetails);
 	}
-}*/
-
-chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
+}
+function start() {
+	
+	chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
 	for (let i = 0, len = addrs.length; i<len; i++){
 		if(addrs[i].tabId==removedTabId){
 			addrs[i].tabId=addedTabId;
@@ -33,26 +35,14 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 			});
 		}
 });
+	
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((info)=>{
+		sendImg(info.request);
+});
 
-function sendImg(requestDetails) {
-	let filt=addrs.filter((adr)=>{return (adr.tabId==requestDetails.tabId && adr.url==requestDetails.url);});
-	if(filt.length==0){
-		chrome.tabs.sendMessage(requestDetails.tabId, {imgSrc: requestDetails.url});
-		addrs.push(requestDetails);
-	}
 }
 
-chrome.webRequest.onBeforeRequest.addListener(
-  sendImg,
-  {urls:["<all_urls>"], types:["image"]},
-  ["blocking","requestBody", "extraHeaders"]
-);
-
-/*chrome.webRequest.onCompleted.addListener(
-  doneAddr,
-  {urls:["<all_urls>"], types:["image"]},
-  ["responseHeaders"]
-);*/
+start();
 
 } catch (e) {
   console.error(e);
